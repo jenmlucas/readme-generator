682426ccbe38d00ef027d7abfd8b4ad5a0846b6e
jenmlucas <jenmlucas1@gmail.com>
2021-11-09 19:51:43 -0700

    added license information

--- index.js
+++ index.js
@@ -4,128 +4,140 @@ const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions=[
-        {
-            type: "confirm",
-            name: "confirmReadMe",
-            message: "Would you like to create a ReadMe?",
-            default: true
-        },
-        {
-            type: "input",
-            name: "readMe",
-            message: "Let's do this!",
-            when: ({confirmReadMe}) => {
-                if (confirmReadMe) {
-                    return true;
-                }else {
-                    console.log("Okay, see you later")
-                    return false;
-                    questions();
-                }
+const questions = [
+    {
+        type: "input",
+        name: "readMe",
+        message: "Let's create a ReadMe. (Press Enter to Begin)",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your Project? (Required)",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log("Please enter a title.");
+                return false;
             }
-        },
-        
-        {
-            type: "input",
-            name: "title",
-            message: "What is the title of your Project? (Required)",
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log("Please enter a title.");
-                    return false;
-                }
+        }
+    },
+    {
+        input: "input",
+        name: "description",
+        message: "Please enter your descritption:",
+    },
+    {
+        type: "confirm",
+        name: "confirmInstall",
+        message: "Would you like to describe the installation process?",
+        default: true
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "Please describe the installation process",
+        when: ({ confirmInstall }) => {
+            if (confirmInstall) {
+                return true;
+            } else {
+                return false;
             }
-        },
-        // {
-        //     type: "confirm",
-        //     name: "confirmDescription",
-        //     message: "Would you like to add a description to your Readme?",
-        //     default: true
-        // },
-        {
-            input: "input",
-            name: "description",
-            message: "Please enter your descritption:",
-            // when: ({ confirmDescription }) => {
-            //     if (confirmDescription) {
-            //         return true;
-            //     } else {
-            //         return false;
-            //     }
-            // }
-        },
-        {
-            type: "confirm",
-            name: "confirmInstall",
-            message: "Would you like to describe the installation process?",
-            default: true
-        },
-        {
-            type: "input",
-            name: "install",
-            message: "Please describe the installation process",
-            when: ({ confirmInstall }) => {
-                if (confirmInstall) {
-                    return true;
-                } else {
-                    return false;
-                }
+        }
+    },
+    {
+        type: "confirm",
+        name: "confirmUsage",
+        message: "Would you like to create a usage section?",
+        default: true
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please describe the usage of this project",
+        when: ({ confirmUsage }) => {
+            if (confirmUsage) {
+                return true;
+            } else {
+                return false;
             }
-        },
-        {
-            type: "confirm",
-            name: "confirmUsage",
-            message: "Would you like to create a usage section?",
-            default: true
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: "Please describe the usage of this project",
-            when: ({confirmUsage}) => {
-                if(confirmUsage) {
-                    return true;
-                } else {
-                    return false;
-                }
+        }
+    },
+    {
+        type: "input",
+        name: "collaborators",
+        message: "Please enter your collaborators"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose a License for this project",
+        choices: [
+            "MIT",
+            "Apache",
+            "GNU GPL v3",
+        ]
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please enter your Test information here"
+    },
+    {
+        type: "input",
+        name: "emailLink",
+        message: "Enter your email. (Required)",
+        validate: emailLink => {
+            if (emailLink) {
+                return true;
+            } else {
+                console.log("Please enter your email!");
+                return false;
             }
-        },
-        {
-            type: "input",
-            name: "collaborators",
-            message: "Please enter your collaborators"
-        },
-        {
-            type: "list",
-            name: "license",
-            message: "Choose a License for this project",
-            choices: [
-                "Apache",
-                "Academic",
-                "GNU",
-                "ISC",
-                "MIT",
-                "Open",
-            ]
-        },
-    ];
+        }
+    },
+    {
+        type: "input",
+        name: "gitHubProjectLink",
+        message: "Enter the Github link to your project. (Required)",
+        validate: gitHubProjectLink=> {
+            if (gitHubProjectLink) {
+                return true;
+            } else {
+                console.log("Please enter the link to your Github project");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "gitHubProfileLink",
+        message: "Enter the Github link to your Profile. (Required)",
+        validate: gitHubProLink=> {
+            if (gitHubProLink) {
+                return true;
+            } else {
+                console.log("Please enter the link to your Github Profile");
+                return false;
+            }
+        }
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { 
-    const inputData= generateMarkdown(data)
+function writeToFile(fileName, data) {
+    const inputData = generateMarkdown(data)
     fs.writeFile(fileName, inputData, (err) => {
-        if (err) {console.log(err)}
+        if (err) { console.log(err) }
     })
 }
 // TODO: Create a function to initialize app
 function init() {
-    inquirer 
-    .prompt(questions)
-    .then((answers) => writeToFile("ProjectReadMe.md", answers))
-    .catch((error) => console.log(error))
+    inquirer
+        .prompt(questions)
+        .then((answers) => writeToFile("ProjectReadMe.md", answers))
+        .catch((error) => console.log(error))
 }
 
 // Function call to initialize app
@@ -145,13 +157,13 @@ init();
 // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests- Done
 
 // WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under- done?
 
 // WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile-?
 
 // WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions- done
 
 // WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
+// THEN I am taken to the corresponding section of the README-?
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,40 +1,106 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-// return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+function renderLicenseBadge(license) {
+  let licenseName = license;
+  if (licenseName === "MIT") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if (licenseName === "Apache") {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (licenseName === "GNU GPL v3") {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  } else {
+    return "N/A";
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = license;
+  if (licenseSection === "MIT") {
+    return `MIT License
+
+    Copyright (c) [year] [fullname]
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.`
+  } else if (licenseSection === "Apache") {
+    return `Copyright [yyyy] [name of copyright owner]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.`
+  } else if (licenseSection === "GNU GPL v3") {
+    return `Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.`
+  } else {
+    return "N/A";
+  }
+};
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
- 
+  return `
+${renderLicenseBadge(data.license)} 
+
+# ${data.title}
+
 ## Description
 ${data.description}
 
 ## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contribution)
+* [Test](#test)
+* [Questions](#questions)
 
 ## Installation
 ${data.install}
 
 ## Usage
 ${data.usage}
+[Github Link](${data.gitHubProjectLink})
 
 ## License
-${renderLicenseBadge(data.license)}
+${renderLicenseSection(data.license)}
 
-## Contributing
+## Contribution
 ${data.collaborators}
 
 ## Tests
+${data.tests}
 
 ## Questions
+Please checkout my [GitHub](${data.gitHubProfileLink}) for other projects. If you have any other questions or concerns, feel free to email me at ${data.emailLink}.
 
 `;
 }
